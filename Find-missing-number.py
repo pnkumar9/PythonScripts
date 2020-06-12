d853441e1daf10765b94798c5af98684b09284a3
Perabattula <kperabattula@deloitte.com>
2020-06-11 20:23:44 -0400

    new file

--- Find-missing-number.py
+++ Find-missing-number.py
@@ -21,4 +21,31 @@ def find_mising_number(arr1,arr2):
 
 print(find_mising_number([1,2,3,4,5,6,7],[7,5,6,2,1,4]))
 
+import collections
+
+def finder2(arr1, arr2): 
+    
+    # Using default dict to avoid key errors
+    d=collections.defaultdict(int) 
+    
+    # Add a count for every instance in Array 1
+    for num in arr2:
+        d[num]+=1 
+    
+    # Check if num not in dictionary
+    for num in arr1: 
+        if d[num]==0: 
+            return num 
+        
+        # Otherwise, subtract a count
+        else: d[num]-=1 
+
+def finder3(arr1, arr2): 
+    result=0 
+    
+    # Perform an XOR between the numbers in the arrays
+    for num in arr1+arr2: 
+        result^=num 
+        print (result)
+    return result         
 
